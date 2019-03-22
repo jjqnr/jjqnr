08ee5f0e631a03ef7f6b6adcc6ccb32e992f11c9
linzhouzhi <490664646@qq.com>
2019-03-22 20:06:32 +0800

    init

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -3,14 +3,10 @@ package com;
  * Created by lzz on 2018/9/11.
  */
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
-@MapperScan(basePackages={"com.common.dao"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/jjqnr/DemoController.java
+++ src/main/java/com/jjqnr/DemoController.java
@@ -1,18 +1,21 @@
-package com.hellosix;
+package com.jjqnr;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lzz on 2018/9/11.
  */
-@Controller
+@RestController
 @RequestMapping("/demo")
 public class DemoController {
 
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
     public String list(@PathVariable("id") String id) {
         return id + "/test";
     }
 }
+
+
